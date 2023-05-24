f6b01b413860438aab6ab688d3a68149f62069c1
Chris Coble <chris@ascentops.com>
2023-05-24 16:21:09 -0400

    Custom target for BPIR2

--- target/linux/mediatek/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/base-files/lib/upgrade/platform.sh
@@ -1,68 +1,74 @@
-platform_do_upgrade() {                 
+#
+# Copyright (C) 2016 OpenWrt.org
+#
+
+platform_do_upgrade() {
+	local tar_file="$1"
+	local board="$(board_name)"
+
+	echo "flashing kernel"
+	tar xf $tar_file sysupgrade-$board/kernel -O | mtd write - kernel
+
+	echo "flashing rootfs"
+	tar xf $tar_file sysupgrade-$board/root -O | mtd write - rootfs
+
+	return 0
+}
+
+platform_check_image() {
+	local tar_file="$1"
 	local board=$(board_name)
+
 	case "$board" in
-	"unielec,u7623"*)
-		#Keep the persisten random mac address (if it exists)
-		mkdir -p /tmp/recovery
-		mount -o rw,noatime /dev/mmcblk0p1 /tmp/recovery
-		[ -f "/tmp/recovery/mac_addr" ] && \
-			mv -f /tmp/recovery/mac_addr /tmp/
-		umount /tmp/recovery
-
-		#1310720 is the offset in bytes from the start of eMMC and to
-		#the location of the kernel (2560 512 byte sectors)
-		get_image "$1" | dd of=/dev/mmcblk0 bs=1310720 seek=1 conv=fsync
-
-		mount -o rw,noatime /dev/mmcblk0p1 /tmp/recovery
-		[ -f "/tmp/mac_addr" ] && mv -f /tmp/mac_addr /tmp/recovery
-		sync
-		umount /tmp/recovery
+	mediatek,mt7623-rfb-nand-ephy |\
+	mediatek,mt7623-rfb-nand)
+		nand_do_platform_check $board $1
+		return $?
 		;;
+	bananapi,bpi-r2 |\
+	mediatek,mt7623-rfb-emmc)
+		local kernel_length=`(tar xf $tar_file sysupgrade-$board/kernel -O | wc -c) 2> /dev/null`
+		local rootfs_length=`(tar xf $tar_file sysupgrade-$board/root -O | wc -c) 2> /dev/null`
+		;;
+
 	*)
-		default_do_upgrade "$1"
+		echo "Sysupgrade is not supported on your board yet."
+		return 1
 		;;
 	esac
-}
 
-PART_NAME=firmware
+	[ "$kernel_length" = 0 -o "$rootfs_length" = 0 ] && {
+		echo "The upgarde image is corrupt."
+		return 1
+	}
 
-platform_check_image() {                                                         
-	local board=$(board_name)                                                
-	local magic="$(get_magic_long "$1")"                                     
-
-	[ "$#" -gt 1 ] && return 1                                               
-
-	case "$board" in                                                       
-	bananapi,bpi-r2|\
-	"unielec,u7623"*)
-		[ "$magic" != "27051956" ] && {   
-			echo "Invalid image type."
-			return 1                                     
-		}                                                    
-		return 0                                             
-		;;                                                   
-
-	*)                                                           
-		echo "Sysupgrade is not supported on your board yet."
-		return 1                                             
-		;;                                
-	esac                                      
-
-	return 0                                                                                         
-}
-
-platform_copy_config_emmc() {
-	mkdir -p /recovery
-	mount -o rw,noatime /dev/mmcblk0p1 /recovery
-	cp -af "$UPGRADE_BACKUP" "/recovery/$BACKUP_FILE"
-	sync
-	umount /recovery
+	return 0
 }
 
 platform_copy_config() {
+	flash_eraseall -j /dev/mtd5
+	if [ $? != 0 ]; then
+		echo "Failed to format device as jffs2"
+		return 1
+	fi
+	
+	mount -t jffs2 -o noatime /dev/mtdblock5 /mnt
+	if [ $? != 0 ]; then
+		echo "Failed to mount device for saving configuration"
+		return 1
+	fi
+
+	cp -af "$CONF_TAR" /mnt/
+	umount /mnt
+
+	return 0
+}
+
+platform_pre_upgrade() {
 	case "$(board_name)" in
-	"unielec,u7623"*)
-		platform_copy_config_emmc
+	mediatek,mt7623-rfb-nand-ephy |\
+	mediatek,mt7623-rfb-nand)
+		nand_do_upgrade $1
 		;;
 	esac
 }
